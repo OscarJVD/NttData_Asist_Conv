4eefab3175755d1ef78b461303a10e46c02dce54
OscarJVD <oscarjaviervargas@hotmail.com>
2021-12-17 19:55:39 -0500

    .

---
+++ READme.txt
@@ -0,0 +1,2 @@
+ejecutar npm run dev
+abrir ruta en https://my.local.host:50000/
---
+++ index.js
@@ -0,0 +1,33 @@
+//archivo index.js
+const express = require('express');
+const fs = require('fs');
+const path = require('path');
+const https = require('https');
+const cons = require('consolidate');
+
+// view engine setup
+const expressApp = express();
+
+expressApp.engine('html', cons.swig);
+expressApp.set("trust proxy", true);
+// expressApp.set("view engine", "html");
+expressApp.set("views", path.resolve(__dirname, "views"));
+expressApp.use(express.static(path.join(__dirname, 'public')));
+
+https.createServer({
+  cert: fs.readFileSync('localhost.crt'),
+  key: fs.readFileSync('localhost.key')
+}, expressApp).listen(50000, function () {
+  console.log('Servidor https correindo en el puerto 50000');
+});
+
+expressApp.get('/', function (req, res) {
+  // res.send('Hola, estas en la pagina inicial');
+  // console.log('Se recibio una petición get a través de https');
+  res.render('index.html');
+});
+
+expressApp.get('/oscar', function (req, res) {
+  res.send('Hola, estas en la pagina sdfsdfsdf');
+  console.log('Se recibio una petición get a través de https');
+});
